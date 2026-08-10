0053e632779e440b5ac023dbabaeed956bb40a26
EduardoQA4 <113567239+EduardoQA4@users.noreply.github.com>
2026-08-09 21:56:04 -0600

    Actualizo config.php
    Se definen dos constantes para la ejecución

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 // Configuración de la base de datos
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'Steven24199');
+define('DB_PASS', 'S0p0rt32021');
 define('DB_NAME', 'tiendaPaluseAmbiente');
 
 // Configuración de la URL base
@@ -10,3 +10,8 @@ $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVE
 $domainName = $_SERVER['HTTP_HOST'];
 $path = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
 define('BASE_URL', $protocol . $domainName . $path);
+
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('ROOT', dirname(APPROOT));
